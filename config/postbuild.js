e1afe3d3f0bc850bb9343cabcda82543299597d5
Laurie <laurieHardingRussell@gmail.com>
2019-10-30 19:32:17 -0600

    Updated to angular 8.

--- config/postbuild.js
+++ config/postbuild.js
@@ -1,4 +1,12 @@
 var fs = require('fs');
 
 fs.createReadStream('docs/index.html').pipe(fs.createWriteStream('docs/404.html'));
-fs.writeFile("docs/CNAME", "gillianharding-russell.ca");
+fs.writeFile("docs/CNAME", "gillianharding-russell.ca",
+    function(err) {
+
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    }
+); 
