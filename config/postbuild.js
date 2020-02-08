7fa4d841d0f4c14ef832c4e70474f342416be0c8
Laurie Harding-Russell <laurieHardingRussell@gmail.com>
2020-02-08 10:59:55 -0600

    Hypothetically created build with bazel.

--- config/postbuild.js
+++ config/postbuild.js
@@ -1,4 +1,7 @@
 var fs = require('fs');
+var copydir = require('copy-dir');
+ 
+copydir.sync('dist/bin/src/prodapp', 'docs');
 
 fs.createReadStream('docs/index.html').pipe(fs.createWriteStream('docs/404.html'));
 fs.writeFile("docs/CNAME", "gillianharding-russell.ca",
